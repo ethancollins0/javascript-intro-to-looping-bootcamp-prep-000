3b04e9cf65e43ed3b61de313ba0974b5978df6d5
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 15:45:08 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,33 @@
+/*for ([initialization]; [condition];
+[iteration]) {
+  [loopBody];
+}*/
+
+for (var i = 1; i < 100; i++) {
+  console.log("Hello World the " + i + "time")
+}
+
+//the above prints Hello World the n time
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i == 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+        array.push(`I am ${i} strange loops.`)
+    }
+  }
+}
+
+let countdown = 100;
+
+while (countdown > 0) {
+  console.log(--countdown);
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(`${number}`)
+    number--;
+  }
+  return 'done'
+}
