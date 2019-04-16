bb0e7c5d20c8cd6839bcc7550fd949631d915c5e
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 16:01:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -34,12 +34,12 @@ function whileLoop(number) {
 }
 
 var i = 0;
- 
+
 function incrementVariable() {
   i = i + 1;
   return i;
 }
- 
+
 do {
   console.log("doo-bee-doo-bee-doo");
 } while (incrementVariable() < 5);
