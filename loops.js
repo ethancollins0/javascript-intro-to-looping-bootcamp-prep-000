fa9bfb6383f12ce3f37a0436304ad4545d3ba3be
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 15:47:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,7 +10,7 @@ for (var i = 1; i < 100; i++) {
 //the above prints Hello World the n time
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i == 1) {
+    if (i === 1) {
       array.push("I am 1 strange loop.")
     } else {
         array.push(`I am ${i} strange loops.`)
